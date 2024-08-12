14651074964b53f9db648f0db71f70898ad14979
lleonardus <leonardus.wizard@gmail.com>
2024-08-12 16:37:04 -0300

    creating popup for audio button when audio is not available

--- src/components/WordDetails.jsx
+++ src/components/WordDetails.jsx
@@ -28,8 +28,13 @@ export default function WordDetails({ wordDetails }) {
         <button
           disabled={!isAudioAvailable}
           onClick={handlePlayAudio}
-          className={`group h-12 w-12 rounded-full sm:h-[75px] sm:w-[75px] ${!isAudioAvailable ? "cursor-not-allowed" : ""}`}
+          className={`group relative h-12 w-12 rounded-full sm:h-[75px] sm:w-[75px] ${!isAudioAvailable ? "cursor-not-allowed" : ""}`}
         >
+          <span
+            className={`absolute bottom-10 right-12 hidden w-max rounded-lg rounded-br-none bg-black/70 px-2 py-1 text-sm text-white sm:bottom-16 sm:right-[75px] dark:bg-gray-800/60 ${!isAudioAvailable ? "group-hover:block" : ""}`}
+          >
+            Not Available
+          </span>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 75 75">
             <g fillRule="evenodd">
               <circle
