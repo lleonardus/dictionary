09e92a390c7079e13c1041c1e237853a28645bfe
lleonardus <leonardus.wizard@gmail.com>
2024-08-12 17:38:48 -0300

    collapsing link to source instead of breaking it into multiple lines

--- src/components/WordDetails.jsx
+++ src/components/WordDetails.jsx
@@ -119,7 +119,11 @@ export default function WordDetails({ wordDetails }) {
       <div className="flex flex-col gap-3 pt-8 underline-offset-2 sm:flex-row sm:pt-6">
         <p className="text-gray-500 underline decoration-gray-500">Source</p>
         <span className="flex items-center gap-[10px] underline decoration-black dark:decoration-white">
-          <a href={wordDetails.sourceUrls[0]} target="_blank">
+          <a
+            className="overflow-hidden text-ellipsis whitespace-nowrap"
+            href={wordDetails.sourceUrls[0]}
+            target="_blank"
+          >
             {wordDetails.sourceUrls[0]}
           </a>
           <svg
