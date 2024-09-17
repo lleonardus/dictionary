dc76072e2fa88a36523df80366e53c8241f46562
lleonardus <leonardus.wizard@gmail.com>
2024-09-16 21:22:34 -0300

    organazing the audios so that us > uk > au

--- src/components/WordDetails.jsx
+++ src/components/WordDetails.jsx
@@ -2,8 +2,24 @@ import ListOfMeanings from "./ListOfMeanings";
 import AudioButton from "./AudioButton";
 
 export default function WordDetails({ wordDetails }) {
-  const audio = wordDetails.phonetics.filter((item) => item.audio)[0];
-  const phonetics = audio?.text;
+  const sortedAudios = wordDetails.phonetics
+    .filter((item) => item.audio)
+    .sort((a, b) => {
+      const order = ["-us.mp3", "-uk.mp3", "-au.mp3"];
+
+      const getSuffixIndex = (audioUrl) => {
+        return order.findIndex((suffix) => audioUrl.endsWith(suffix));
+      };
+
+      const aSuffixIndex = getSuffixIndex(a.audio);
+      const bSuffixIndex = getSuffixIndex(b.audio);
+
+      return aSuffixIndex - bSuffixIndex;
+    });
+
+  const audioWithText = sortedAudios.find((audio) => audio.text);
+
+  const phonetics = audioWithText ? audioWithText.text : "";
   const meanings = wordDetails.meanings.reduce((acc, current) => {
     const existing = acc.find(
       (item) => item.partOfSpeech === current.partOfSpeech,
@@ -35,7 +51,7 @@ export default function WordDetails({ wordDetails }) {
             {phonetics || wordDetails.phonetic}
           </p>
         </div>
-        <AudioButton audio={audio?.audio} />
+        <AudioButton audio={audioWithText?.audio || sortedAudios[0]?.audio} />
       </div>
       <div className="border-b border-gray-400 pb-10 sm:pb-11 dark:border-gray-600">
         {meanings.map((meanings, index) => {
